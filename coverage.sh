6c4e088c8c04af4381e8180999a9924e57cf72c1
John J. Camilleri <john@johnjcamilleri.com>
2013-04-18 19:12:42 +0200

    Fix coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Count missing functions and return as percentage of all functions in RGL
-MISSING=`echo "pg -missing | ? wc -w" | gf --run AllMlt.gf | cut -f1 -d' '`
+MISSING=`echo "pg -missing" | gf --run AllMlt.gf | wc -w | sed -e 's/^[ \t]*//'`
 TOTAL=690
 PERC=$((100*MISSING/TOTAL))
 echo "Missing: ${MISSING}/${TOTAL} (${PERC}%)"
